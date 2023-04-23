3168e23a43620b40a478baf8f82d69ff61479f73
LesCamposR <lesthercamposramirez91@gmail.com>
2023-04-23 22:27:02 +0000

    Undo

--- docs/assets/reset_migrations_local_postgre.bash
+++ docs/assets/reset_migrations_local_postgre.bash
@@ -1,7 +1,7 @@
 rm -R -f ./migrations &&
 pipenv run init &&
-psql -U lescampos -c 'DROP DATABASE example;' || true &&
-psql -U lescampos -c 'CREATE DATABASE example;' &&
-psql -U lescampos -c 'CREATE EXTENSION unaccent;' -d example &&
+psql -U postgres -c 'DROP DATABASE example;' || true &&
+psql -U postgres -c 'CREATE DATABASE example;' &&
+psql -U postgres -c 'CREATE EXTENSION unaccent;' -d example &&
 pipenv run migrate &&
 pipenv run upgrade
