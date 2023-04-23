dbddd9f45c848fd937748d6a51f9827773a3213b
Stephania31 <acunaantunez@gmail.com>
2023-04-23 23:41:29 +0000

    genero y apellido formulario de registro

--- src/front/js/component/registerForm.jsx
+++ src/front/js/component/registerForm.jsx
@@ -5,28 +5,41 @@ import "../../styles/home.css";
 
 const RegisterForm = () => {
   const [username, setUsername] = useState("");
+  const [lastName, setLastName] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
   const [birthday, setBirthday] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
+  const [gender, setGender] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(
-      `Username: ${username}, Password: ${password}, Email: ${email}, Phone Number: ${phoneNumber}`
+      `Username: ${username},  Last Name: ${lastName}, Password: ${password}, Email: ${email}, Phone Number: ${phoneNumber}, Gender: ${gender}`
     );
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <div>
-        <label htmlFor="username">Username:</label>
+        <label className="userName" htmlFor="username">
+          Username:
+        </label>
         <input
           type="text"
           id="username"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
         />
+        <div>
+          <label htmlFor="lastName">Last Name:</label>
+          <input
+            type="text"
+            id="lastName"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+        </div>
       </div>
       <div>
         <label htmlFor="password">Password:</label>
@@ -64,6 +77,20 @@ const RegisterForm = () => {
           onChange={(e) => setPhoneNumber(e.target.value)}
         />
       </div>
+      <div>
+        <label htmlFor="gender">Gender:</label>
+        <select
+          id="gender"
+          value={gender}
+          onChange={(e) => setGender(e.target.value)}
+        >
+          <option value="">Select gender</option>
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+          <option value="other">Other</option>
+        </select>
+      </div>
+
       <button type="submit">Register</button>
     </form>
   );
