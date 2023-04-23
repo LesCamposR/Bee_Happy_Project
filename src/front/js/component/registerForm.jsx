3322ba91a0b4a9ea7f8ee436974219e12352bb6d
Stephania31 <acunaantunez@gmail.com>
2023-04-23 22:23:23 +0000

    agregando tel

--- src/front/js/component/registerForm.jsx
+++ src/front/js/component/registerForm.jsx
@@ -7,6 +7,15 @@ const RegisterForm = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
+  const [birthday, setBirthday] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(
+      `Username: ${username}, Password: ${password}, Email: ${email}, Phone Number: ${phoneNumber}`
+    );
+  };
 
   return (
     <form onSubmit={handleSubmit}>
@@ -35,6 +44,24 @@ const RegisterForm = () => {
           id="email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="birthday">Birthday:</label>
+        <input
+          type="text"
+          id="username"
+          value={birthday}
+          onChange={(e) => setBirthday(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="phoneNumber">Phone Number:</label>
+        <input
+          type="tel"
+          id="phoneNumber"
+          value={phoneNumber}
+          onChange={(e) => setPhoneNumber(e.target.value)}
         />
       </div>
       <button type="submit">Register</button>
