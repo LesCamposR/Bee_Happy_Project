e1f36512e1d0b460d01ac8e117dd15019cdd371e
LesCampos <lescampos@LesCamposs-MacBook-Pro.local>
2023-04-23 17:08:47 -0600

    singleproductscard

--- docs/assets/reset_migrations_local_postgre.bash
+++ docs/assets/reset_migrations_local_postgre.bash
@@ -1,7 +1,7 @@
 rm -R -f ./migrations &&
 pipenv run init &&
-psql -U lescampos -c 'DROP DATABASE example;' || true &&
-psql -U lescampos -c 'CREATE DATABASE example;' &&
-psql -U lescampos -c 'CREATE EXTENSION unaccent;' -d example &&
+psql -U LesCampos -c 'DROP DATABASE example;' || true &&
+psql -U LesCampos -c 'CREATE DATABASE example;' &&
+psql -U LesCampos -c 'CREATE EXTENSION unaccent;' -d example &&
 pipenv run migrate &&
 pipenv run upgrade
--- src/front/js/component/Singleproducts.jsx
+++ src/front/js/component/Singleproducts.jsx
@@ -2,17 +2,17 @@ import React, { useState, useEffect, useContext } from "react";
 import { Link, useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
 
-const People = () => {
+const SingleProducts = () => {
     const { store, actions } = useContext(Context);
     const params = useParams();
-    const [people, setPeople] = useState({})
+    const [singleproducts, setsingleproducts] = useState({})
 
     useEffect(() => {
         const cargaDatos = async () => {
-            let { respuestaJson, response } = await actions.useFetch(`/people/${params.uid}`)
+            let { respuestaJson, response } = await actions.useFetch(`/singleproducts/${params.uid}`)
             if (response.ok) {
                 console.log(respuestaJson)
-                setPeople(respuestaJson.result.properties)
+                setsingleproducts(respuestaJson.result.properties)
             }
         }
         cargaDatos()
@@ -52,6 +52,23 @@ const People = () => {
                 </div>
             </div>
         </div>
+        <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">An item</li>
+                <li class="list-group-item">A second item</li>
+                <li class="list-group-item">A third item</li>
+            </ul>
+            <div class="card-body">
+                <a href="#" class="card-link">Card link</a>
+                <a href="#" class="card-link">Another link</a>
+            </div>
+        </div>
+
     </>)
 }
 
