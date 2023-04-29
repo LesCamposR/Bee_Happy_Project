91fe7b4b275051939f079370e791b7e9e768fd2c
LesCamposR <lesthercamposramirez91@gmail.com>
2023-04-29 03:12:20 +0000

    Models2

--- src/api/models.py
+++ src/api/models.py
@@ -14,7 +14,7 @@ class User(db.Model):
     birth_date = db.Column(db.Date, unique=False, nullable=False)
     address = db.Column(db.String(200), unique=False, nullable=False)
     country = db.Column(db.String(120), unique=False, nullable=True)
-    last_login = db.Column(db.date, unique=False, nullable=False)
+    last_login = db.Column(db.Date, unique=False, nullable=False)
     email_recover = db.Column(db.String(120), unique=False, nullable=False)
     securityQA1 = db.Column(db.String(120), unique=False, nullable=False)
     securityQA2 = db.Column(db.String(120), unique=False, nullable=False)
@@ -35,7 +35,7 @@ class User(db.Model):
 class Shoppingcart(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable= False)
-    Products_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable= False)
+    Products_id = db.Column(db.Integer, db.ForeignKey('products.id'), nullable= False)
     Products_status = db.Column(db.String(120), unique=True, nullable=False)
     def __repr__(self):
         return f'<Shoppingcart {self.email}>'
@@ -54,9 +54,8 @@ class ClassProduct(db.Model):
     Price = db.Column(db.Float, unique=False, nullable=False)
     Description = db.Column(db.String(200), unique=False, nullable=False)
     Rating = db.Column(db.Float, unique=False, nullable=False)
-    Reviews = 
-    Share_Times =
-    Stock =
+    Reviews = db.Column(db.Integer, unique=False, nullable=False)
+    Stock = db.Column(db.Integer, unique=False, nullable=False)
     def __repr__(self):
         return f'<ClassProduct {self.email}>'
 
@@ -70,10 +69,10 @@ class ShoppingHistory(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     User_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable= False)
     Order_id = db.Column(db.Integer, db.ForeignKey('Order.id'), nullable= False)
-    Products_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable= False)
+    Products_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable= False)
     Invoice_Number = db.Column(db.Float, unique=False, nullable=False)
     Status = db.Column(db.String(120), unique=True, nullable=False)
-        def __repr__(self):
+    def __repr__(self):
         return f'<ShoppingHistory {self.email}>'
 
     def serialize(self):
