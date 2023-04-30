931821f647e0300e79c91edc5b96a23934f0ff1b
LesCamposR <lesthercamposramirez91@gmail.com>
2023-04-30 04:39:52 +0000

    routesUpdate

--- src/api/routes.py
+++ src/api/routes.py
@@ -37,3 +37,190 @@ def handle_hola():
     }
 
     return jsonify(response_body), 200
+
+
+# Handle/serialize errors like a JSON object
+@api.errorhandler(APIException)
+def handle_invalid_usage(error):
+    return jsonify(error.to_dict()), error.status_code
+
+# generate sitemap with all your endpoints
+@api.route('/')
+def sitemap():
+    return generate_sitemap(app)
+
+# Register*************************
+# Register*************************
+
+@api.route('/register', methods=['POST'])
+def register_user():
+    #recibir el body en json, des-jsonificarlo y almacenarlo en la variable body
+    body = request.get_json() #request.json() pero hay que importar request y json
+
+    #ordernar cada uno de los campos recibidos
+    email = body["email"]
+    name = body["name"]
+    password = body["password"]
+    is_active = body["is_active"]
+
+    #validaciones
+    if body is None:
+        raise APIException("You need to specify the request body as json object", status_code=400)
+    if "email" not in body:
+        raise APIException("You need to specify the email", status_code=400)
+    #encriptamos el password en la base de datos
+    password_encrypted = bcrypt.generate_password_hash(password, 10).decode('utf-8')
+
+    #creada la clase User en la variable new_user
+    new_user = User(email=email, name=name, password=password_encrypted, is_active=is_active)
+
+    #comitear la sesión
+    db.session.add(new_user) #agregamos el nuevo usuario a la base de datos
+    db.session.commit() #guardamos los cambios en la base de datos
+
+    return jsonify({"mensaje":"NewUser Register Correctly"}), 201
+
+
+# User*************************
+# User*************************
+# User*************************
+
+@api.route('/user', methods=['GET'])
+def get_users():
+    users = User.query.all()  #<User Antonio>
+    users = list(map(lambda item: item.serialize(), users)) #{name:Antonio, password:123, ....} {name:Usuario2, password:123.... }
+    print(users)
+  
+    return jsonify(users), 200
+
+@api.route('/get-user/<int:id>', methods=['GET'])
+def get_specific_user(id):
+    user = User.query.get(id)    
+  
+    return jsonify(user.serialize()), 200
+
+@api.route('/get-user', methods=['POST'])
+def get_specific_user2():
+    body = request.get_json()   
+    id = body["id"]
+
+    user = User.query.get(id)   
+  
+    return jsonify(user.serialize()), 200
+
+@api.route('/get-user', methods=['DELETE'])
+def delete_specific_user():
+    body = request.get_json()   
+    id = body["id"]
+
+    user = User.query.get(id) 
+
+    db.session.delete(user)
+    db.session.commit()  
+  
+    return jsonify("Usuario borrado"), 200
+
+@api.route('/get-user', methods=['PUT'])
+def edit_user():
+    body = request.get_json()   
+    id = body["id"]
+    name = body["name"]
+
+    user = User.query.get(id)   
+    user.name = name #modifique el nombre del usuario
+
+    db.session.commit()
+  
+    return jsonify(user.serialize()), 200
+
+
+# Login*************************
+# Login*************************
+
+
+@api.route("/login", methods=["POST"])
+def login():
+    email = request.get_json()["email"]
+    password = request.get_json()["password"]
+
+    userEmailLogin = User.query.filter_by(email=email).first()
+
+    if user is None:
+        return jsonify({"message":"Login failed"}), 401
+
+ #Esta veificaicon es para el inicio de la aplicacion Go all the way Up
+    """ if password != user.password:
+        return jsonify({"message":"Login failed"}), 401 """
+        
+ #validar el password encriptado / esto devuelve True si hay coincidencia 
+    if not bcrypt.check_password_hash(user.password, password):
+        return jsonify({"message":"Login failed"}), 401
+
+# create_access_token() function is used to actually generate the JWT.
+    access_token = create_access_token(identity=user.id)
+    return jsonify({"token":access_token}), 200
+
+
+# Logout*************************
+# Logout*************************
+
+
+@api.route("/logout", methods=["GET"])
+@jwt_required()
+def logout():
+    jti = get_jwt()["jti"] #Identificador del JWT (es más corto)
+    now = datetime.now(timezone.utc) 
+
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    user = User.query.get(current_user)
+
+    tokenBlocked = TokenBlockedList(token=jti , created_at=now, email=user.email)
+    db.session.add(tokenBlocked)
+    db.session.commit()
+
+    return jsonify({"message":"successfully loggedout "})
+
+# ShoppingCart*************************
+# ShoppingCart*************************
+
+@api.route('/ShoppingCart/<int:user_id>', methods=['GET'])
+@jwt_required()
+def get_favorites(user_id):
+    user = User.query.get(user_id)
+    if not user:
+        raise APIException('User not found', status_code=404)
+
+    favorite_people = list(map(lambda item: item.serialize()["people_name"], FavoritePeople.query.filter_by(user_id=user.id)))
+    favorite_planets = list(map(lambda item: item.serialize()["planet_name"], FavoritePlanets.query.filter_by(user_id=user.id)))
+    favorite_vehicles = list(map(lambda item: item.serialize()["vehicle_name"], FavoriteVehicles.query.filter_by(user_id=user.id)))
+
+    return jsonify({
+        "msg":"ok",
+        "all_ShoppingCart": favorite_people + favorite_planets + favorite_vehicles,
+        "favorite_people": favorite_people,
+        "favorite_planets": favorite_planets,
+        "favorite_vehicles": favorite_vehicles
+    }), 200
+
+
+# Protect a route with jwt_required, which will kick out requests
+# without a valid JWT present.
+@api.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    user = User.query.get(current_user)
+    print("UserName:", user.name)
+    return jsonify({"Msg":"This is a protected route"}), 200
+
+
+
+
+
+
+# this only runs if `$ python src/app.py` is executed
+if __name__ == '__main__':
+    PORT = int(os.environ.get('PORT', 3000))
+    app.run(host='0.0.0.0', port=PORT, debug=False)
