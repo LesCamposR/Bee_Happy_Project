c8849a69b25770aa25657043a79b29202be82f1c
jacob0589 <alonsocb8995@gmail.com>
2023-05-16 01:49:08 +0000

    actualizado

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,37 +1,27 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import abeja1 from "../../img/abeja-guerrera.jpg";
+import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => {
-  const [randomMsg, setRandomMsg] = useState("");
-  const [randomImage, setRandomImage] = useState("");
-
-  const messages = [
-    "Soy una abeja guerrera",
-    "Soy una abeja reina",
-    "Soy una abeja trabajadora",
-    "Soy una abeja entusiasta",
-  ];
-
-  const images = [abeja1, imagen2, imagen3, imagen4];
-
-  const generateRandomMsg = () => {
-    const randomMsgIndex = Math.floor(Math.random() * messages.length);
-    const randomMsg = messages[randomMsgIndex];
-    const randomImage = images[randomMsgIndex];
-
-    setRandomMsg(randomMsg);
-    setRandomImage(randomImage);
-  };
+  const { store, actions } = useContext(Context);
 
   return (
-    <div>
-      <button onClick={generateRandomMsg}>GET A MSG</button>
-      <p>{randomMsg}</p>
-      {randomImage && <img src={randomImage} alt="Imagen" />}
+    <div className="text-center mt-5">
+      <h1>Hello Bee Happy!!</h1>
+      <p>
+        <img src={rigoImageUrl} />
+      </p>
+      <div className="alert alert-info">
+        {store.message ||
+          "Loading message from the backend (make sure your python backend is running)..."}
+      </div>
+      <p>
+        This boilerplate comes with lots of documentation:{" "}
+        <a href="https://start.4geeksacademy.com/starters/react-flask">
+          Read documentation
+        </a>
+      </p>
     </div>
   );
 };
-
-export default Home;
