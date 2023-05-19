02a378f2156c0f28f016671b4ac63fc7828b59d7
LesCamposR <lesthercamposramirez91@gmail.com>
2023-05-19 21:08:30 +0000

    Beneficios

--- src/front/js/component/Benefits.jsx
+++ src/front/js/component/Benefits.jsx
@@ -9,9 +9,9 @@ const Benefits = () => {
             <div className="col-4 bg-light justify-content-center mt-4 p-4">
                 <div className="card">
                     <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">T</p>
-                        <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                        <h3 className="card-title">¿Qué beneficios para la salud trae consumir miel?</h3>
+                        <p className="card-text">“La miel es un alimento nutritivo, saludable y natural producido por las abejas. Sus propiedades benéficas van más allá del uso como dulcificante, ya que es rico en sales minerales, enzimas, vitaminas y proteínas que le donan propiedades nutritivas y organolépticas únicas”</p>
+                        <p className="card-text"><small className="text-muted">Se trata de un alimento que se consume desde la antigüedad y al que se le adjudican numerosas bondades.</small></p>
                     </div>
                     <img src={frames2} className="card-img-bottom" alt="..." />
                 </div>
@@ -20,38 +20,39 @@ const Benefits = () => {
             <div className="col-8 bg-light justify-content-center mt-4 p-4">
                 <div class="accordion" id="accordionPanelsStayOpenExample">
                     <div class="accordion-item">
-                        <h2 class="accordion-header" id="panelsStayOpen-headingOne">
-                            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
-                                Accordion Item #1
+                        <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
+                                <h4>Colabora en el tratamiento de las heridas:</h4>
                             </button>
                         </h2>
-                        <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
+                        <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
                             <div class="accordion-body">
-                                <strong>This is the first item's accordion body.</strong> .
+                                <strong>La Miel de Abeja</strong> se ha utilizado a lo largo de los años para tratar heridas, picaduras de insectos, quemaduras, trastornos de la piel, llagas
                             </div>
                         </div>
                     </div>
                     <div class="accordion-item">
                         <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
                             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
-                                Accordion Item #2
+                                <h4>Presenta beneficios gastrointestinales:</h4>
                             </button>
                         </h2>
                         <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
                             <div class="accordion-body">
-                                <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                                <strong>La Miel de Abeja</strong> está compuesta por enzimas que facilitan la absorción de moléculas, como azúcares y almidón. Además, este producto “proporciona algunos nutrientes, como minerales, fitoquímicos y flavonoides, que ayudan a los procesos digestivos del cuerpo”. Incluso la miel pura tiene propiedades bactericidas contra bacterias patógenas y enteropatógenos.
+
                             </div>
                         </div>
                     </div>
                     <div class="accordion-item">
                         <h2 class="accordion-header" id="panelsStayOpen-headingThree">
                             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
-                                Accordion Item #3
+                                <h4>Es efectiva contra la tos y el dolor de garganta:</h4>
                             </button>
                         </h2>
                         <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
                             <div class="accordion-body">
-                                <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                                <strong>La Miel de Abeja</strong> puede ser eficaz en el tratamiento del dolor de garganta gracias a sus propiedades antiinflamatorias, antivirales y antifúngicas. A su vez, “la miel es superior a otros tratamientos para la tos inducida por infecciones de las vías respiratorias superiores”.
                             </div>
                         </div>
                     </div>
