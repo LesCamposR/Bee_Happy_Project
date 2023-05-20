6f4aea06eeeddcd64a4d611a95f57f8e17b67ce4
Jonathan A. Montero C <dyatroniccr@gmail.com>
2023-05-20 22:07:41 +0000

    Update Sthep

--- src/front/js/pages/contact.js
+++ src/front/js/pages/contact.js
@@ -6,79 +6,6 @@ import background from "../../img/Sunset.jpg";
 export const Contact = () => {
     const [message, setMessage] = useState("");
 
-<<<<<<< HEAD
-    return ( <
-        div style = {
-            {
-                backgroundImage: `url(${background})`,
-            }
-        } >
-        <
-        div className = "container mt-5" >
-        <
-        div className = " row justify-content-center mt-5" >
-        <
-        h3 className = "row title justify-content-center text-white" > {
-            " "
-        }
-        Contact {
-            " "
-        } <
-        /h3>{" "} <
-        div className = "Contanct col-sm-4 rounded-3 m-3 p-3 border" >
-        <
-        form >
-        <
-        label className = "form-label fs-5" > Full Name < /label>{" "} <
-        input type = "text"
-        className = "form-control mb-3"
-        placeholder = "Enter Full Name" /
-        >
-        <
-        label className = "form-label fs-5" > Email < /label>{" "} <
-        input type = "email"
-        className = "form-control mb-3"
-        placeholder = "Enter email" /
-        >
-        <
-        label htmlFor = "phone"
-        className = "form-label fs-5" >
-        Phone {
-            " "
-        } <
-        /label>{" "} <
-        input type = "text"
-        className = "form-control mb-3"
-        placeholder = "Enter phone" /
-        >
-        <
-        label htmlFor = "phone"
-        className = "form-label fs-5" >
-        Comentario {
-            " "
-        } <
-        /label>{" "} <
-        textarea type = "text-area"
-        class = "form-control mb-3"
-        placeholder = "Enter your commit" >
-        < /textarea>{" "} <
-        div className = "d-flex justify-content-center" >
-        <
-        button type = "button"
-        className = "button-save col-md-6 btn btn-outline-light fs-6 fw-bold" >
-        Enviar {
-            " "
-        } <
-        /button>{" "} <
-        /div>{" "} <
-        /form>{" "} <
-        /div>{" "} <
-        /div>{" "} <
-        /div>{" "} <
-        /div>
-    );
-};
-=======
   return (
     <div className="container mt-5">
       <div className="row justify-content-center mt-5">
@@ -127,4 +54,3 @@ export const Contact = () => {
     </div>
   );
 };
->>>>>>> Jonathan
